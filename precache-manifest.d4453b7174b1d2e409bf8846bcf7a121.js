988ccfc9a051985f1ab15ab9d9fe6722ed997919
Serena Girardi <serena.girardi@accurat.it>
2020-03-10 23:30:51 +0100

    Updates

---
+++ precache-manifest.d4453b7174b1d2e409bf8846bcf7a121.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "156b53aebfb719a2f264a3daae9485c4",
+    "url": "/spotify-charts-map/index.html"
+  },
+  {
+    "revision": "18b4453b41ebb8cbeea6",
+    "url": "/spotify-charts-map/static/css/main.e1c84a16.chunk.css"
+  },
+  {
+    "revision": "36fb5b9315570d2c0a49",
+    "url": "/spotify-charts-map/static/js/2.d18243bb.chunk.js"
+  },
+  {
+    "revision": "f32f3f2739f945f6e8e72f406ee3a70f",
+    "url": "/spotify-charts-map/static/js/2.d18243bb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "18b4453b41ebb8cbeea6",
+    "url": "/spotify-charts-map/static/js/main.9df461ae.chunk.js"
+  },
+  {
+    "revision": "63dfb3f53259ed5f88fb",
+    "url": "/spotify-charts-map/static/js/runtime-main.82f5550c.js"
+  },
+  {
+    "revision": "184453072f4d631c731fde9092266d17",
+    "url": "/spotify-charts-map/static/media/close.18445307.svg"
+  },
+  {
+    "revision": "dbfe7e691e8cda611fcf249fafa9364a",
+    "url": "/spotify-charts-map/static/media/download.dbfe7e69.svg"
+  }
+]);
